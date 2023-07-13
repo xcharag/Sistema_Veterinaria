77d68f57f1ea091bbf33ff96bbf545de622e034f
Oscar Aguilar Soliz <a2021110028@estudiantes.upsa.edu.bo>
2023-07-13 13:08:46 -0400

    Property Changed Method created

---
+++ Core/ObsewrvableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vet_la_finca.Core
+{
+    internal class ObsewrvableObject : INotifyPropertyChanged //This Inotify is for updating the UI when biding
+    {
+        public event PropertyChangedEventHandler? PropertyChanged; //This implements the PropertyChanged method
+
+        protected void onpropertychanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
