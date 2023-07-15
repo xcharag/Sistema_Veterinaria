a7071ea847d2cde6e6eb11e7465c5c7ac234e97d
Oscar Aguilar Soliz <a2021110028@estudiantes.upsa.edu.bo>
2023-07-15 13:14:33 -0400

    Diseno de la primer vista modelo

--- MWM/ViewModel/MainViewModel.cs
+++ MWM/ViewModel/MainViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vet_la_finca.Core;
 
 namespace Vet_la_finca.MWM.ViewModel
 {
-    internal class MainViewModel
+    internal class MainViewModel : ObservableObject
     {
+        public MainViewModel() { }
     }
 }
