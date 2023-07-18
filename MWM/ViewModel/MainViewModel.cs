358511e231ade2e75660428355ae61d1c42fd0df
Oscar Aguilar Soliz <a2021110028@estudiantes.upsa.edu.bo>
2023-07-18 17:10:16 -0400

    Navegacion 3 pestanas

--- MWM/ViewModel/MainViewModel.cs
+++ MWM/ViewModel/MainViewModel.cs
@@ -9,6 +9,39 @@ namespace Vet_la_finca.MWM.ViewModel
 {
     internal class MainViewModel : ObservableObject
     {
-        public MainViewModel() { }
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand VentasViewCommand { get; set; }
+
+        public RelayCommand BanosViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVm { get; set; }
+
+        public VentasViewModel VentasVm { get; set; }
+
+        public BanosViewModel BanosVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; onpropertychanged(); }
+        }
+
+        public MainViewModel() 
+        {
+            HomeVm = new HomeViewModel();
+            VentasVm = new VentasViewModel();
+            BanosVm = new BanosViewModel();
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o => { CurrentView = HomeVm; });
+
+            VentasViewCommand = new RelayCommand(o => { CurrentView = VentasVm; });
+
+            BanosViewCommand = new RelayCommand(o => { CurrentView = BanosVm; });
+        }
     }
 }
